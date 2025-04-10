dd2d4f9532f873e9527164b2f93b0ecb719f907f
Irasov <srnan@yandex.ru>
2025-04-10 19:48:45 +0500

    feat: (lesson#10) add backend search and pizza pagination with division into 4 pizzas on the screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
 const Home = ({searchValue}) => {
   const [items, setItmes] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const[categoryId, setCategoryId] = React.useState(0);
+  const [currentpage, setCurrentPage] = React.useState(1);
   const[sortType, setSortType] = React.useState(
     {name: 'популярности', sortProperty: 'rating'}
   );
@@ -17,8 +19,9 @@ const Home = ({searchValue}) => {
     const sortBy = sortType.sortProperty.replace('-','');
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}`: '';
+    const search = searchValue ? `&search=${searchValue}` : '';
     fetch(
-      `https://67ee8820c11d5ff4bf79f1be.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://67ee8820c11d5ff4bf79f1be.mockapi.io/items?page=${currentpage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
     )
       .then((response) => {
         return response.json();
@@ -28,16 +31,11 @@ const Home = ({searchValue}) => {
         setIsLoading(false);
       });
       window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentpage]);
 
-  const pizzas = items.filter((obj) => {
-    if(obj.title.toLowerCase().includes(searchValue.toLowerCase())){
-      return true;
-    }
-    return false;
-  }).map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
-  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+  const skeletons = [...new Array(4)].map((_, index) => <Skeleton key={index} />);
 
   return (
     <div className='container'>
@@ -49,6 +47,7 @@ const Home = ({searchValue}) => {
       <div className="content__items">
         {isLoading ? skeletons : pizzas}
       </div>
+      <Pagination onChangePage = {number => setCurrentPage(number)} />
     </div>
   )
 }
