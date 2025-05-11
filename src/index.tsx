9cd635be8db63c617d123da5c69078960c6df573
Irasov <srnan@yandex.ru>
2025-05-11 17:52:06 +0500

    refactor: add baseName for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename=' https://irasov.github.io/react-pizzav2'>
     <Provider store={store}>
       <App />
     </Provider>
